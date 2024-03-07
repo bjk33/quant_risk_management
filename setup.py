5593a5a7a56e38a519093e3f2ca8b3314c1ccfa4
brandonkaplan <bkaps29@gmail.com>
2024-03-07 14:01:12 -0500

    Pushed version 0.1.3 of quant_risk_lib-0.1.3-py3-none-any.whl

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='quant_risk_lib',
     packages=find_packages(include=['quant_risk_lib']),
-    version='0.1.2',
+    version='0.1.3',
     description='My FINTECH545 Python library',
     author='Brandon Kaplan',
     install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'statsmodels']
